8ea2f5e0868267131dbd865804befd6faf75db2e
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-05-19 00:11:14 -0300

    Foi criado o que será o backend, onde teremos o método get para fazer a solicitação dos posts e o método post para criar uma nova postagem. Ambos os métodos ainda devem ser implementados, foi feito apenas o escopo.

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+let posts = [
+    {   id:'lsdkmflksd',
+        title:'Teste do Mural',
+        description:'Aqui vai ter o conteúdo do mural'}
+]
+
+app.get('/all', (req, res)=>{
+    res.json(JSON.stringify(posts))
+})
+
+app.post('/new', (req, res)=>{
+
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server runnin on PORT ${PORT}.`)
 })
