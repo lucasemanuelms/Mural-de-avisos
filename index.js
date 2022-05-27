ffbb70d08482f920c098b22aaeeefb542d5ace35
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-05-26 23:16:36 -0300

    Os métodos de chamada de api foram movidos para uma pasta de rotas para que houvesse uma separação, dessa forma eu garanto que as minhas chamadas para api não façam uma busca para uma pasta chamada api.

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const PORT = 3000
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const posts = require('./model/posts')
+
 const path = require("path")
+const apiRoute = require("./routes/api")
 
+
+/*Para garantir que uma chamada a api não busque por uma pasta chamada api, devemos usar na ordem abaixo*/
+app.use('/api', apiRoute)
 app.use('/', express.static(path.join(__dirname, "public")))
-
-app.get('/all', (req, res)=>{
-    res.json(JSON.stringify(posts.getAll()))
-})
-
-app.post('/new', bodyParser.json(), (req, res)=>{
-    let title = req.body.title
-    let description = req.body.description
-
-    posts.newPost(title, description)
-
-    res.send("Post adicionado!")
-
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server runnin on PORT ${PORT}.`)
