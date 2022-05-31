686a5094cffac0622e069ac3014bd0229f58b6ea
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-05-31 20:50:14 -0300

    Foi adicionado o cors, para tratar o caso das rotas.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 function updatePosts(){
     
-    fetch("http://localhost:3000/api/all").then(res=>{
+    fetch("http://192.168.0.6:3000/api/all").then(res=>{
         return res.json()
     }).then(json=>{
         let postElements = ''
@@ -49,7 +49,7 @@ function newPost(){
         body: JSON.stringify(post)
     }
 
-    fetch("http://localhost:3000/api/new", options).then(res=>{
+    fetch("http://192.168.0.6:3000/api/new", options).then(res=>{
         console.log(res)
         updatePosts()
         document.getElementById('title').value = ''
